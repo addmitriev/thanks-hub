33178ded3c6e897a110c3e7847ae0c74060f3a2f
Eugene Mercuriev <eugene.mercuriev@gmail.com>
2014-10-11 16:49:17 +0400

    add bootstrap and app's root

--- src/main/java/net/thankhub/server/controller/ApiController.java
+++ src/main/java/net/thankhub/server/controller/ApiController.java
@@ -1,17 +1,32 @@
 package net.thankhub.server.controller;
 
+import com.yandex.money.api.methods.RequestPayment;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
+
+import java.io.IOException;
+import java.util.Map;
 
 @Controller
 public class ApiController {
 
-    @RequestMapping(value = "/api/user/{user}/commit/{commit}")
     @ResponseBody
+    @RequestMapping(value = "/api/user/{user}/commit/{commit}")
     public String getWallet(@PathVariable("user") String user, @PathVariable("commit") String commit) {
         return "{clientId: 12321312}";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/api/auth/", method = RequestMethod.GET)
+    public String auth(WebRequest request) throws IOException {
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(parameterMap);
     }
 }
