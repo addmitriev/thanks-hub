209543bf7f0497800a886434844d5848b4498873
Eugene Mercuriev <eugene.mercuriev@gmail.com>
2014-10-11 22:47:31 +0400

    MVP

--- src/main/java/net/thankhub/server/controller/MainController.java
+++ src/main/java/net/thankhub/server/controller/MainController.java
@@ -1,5 +1,6 @@
 package net.thankhub.server.controller;
 
+import net.thankhub.server.service.YaService;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.Map;
 
@@ -15,8 +17,17 @@ import java.util.Map;
 public class MainController {
 
     @RequestMapping(value = {"/form", "/form/"}, method = RequestMethod.GET)
-    public String start(@RequestParam("user") String user, Model model) throws IOException {
-        model.addAttribute("user", user);
-        return "form";
+    public String start(@RequestParam("user") String gitHubUser, Model model, HttpServletRequest request) throws IOException {
+        model.addAttribute("client_id", YaService.CLIENT_ID);
+        model.addAttribute("response_type", "code");
+        model.addAttribute("redirect_uri", YaService.REDIRECT_URI);
+        model.addAttribute("scope", "account-info operation-details payment-p2p");
+        model.addAttribute("instance_name", gitHubUser);
+        model.addAttribute("gitHubUser", gitHubUser);
+
+        request.getSession().setAttribute("gitHubUser", gitHubUser);
+
+        return "allow-form";
     }
+
 }
--- src/test/java/net/thankhub/YaMoneyTest.java
+++ src/test/java/net/thankhub/YaMoneyTest.java
@@ -1,21 +1,20 @@
 package net.thankhub;
 
-import com.yandex.money.api.YandexMoney;
 import com.yandex.money.api.exceptions.InsufficientScopeException;
 import com.yandex.money.api.exceptions.InvalidRequestException;
 import com.yandex.money.api.exceptions.InvalidTokenException;
-import com.yandex.money.api.methods.*;
+import com.yandex.money.api.methods.BaseRequestPayment;
+import com.yandex.money.api.methods.RequestPayment;
 import net.thankhub.server.service.YaService;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.HashMap;
 
 public class YaMoneyTest implements ApiTest2 {
 
-    @Test
+    @Test(enabled = false)
     public void test() throws InvalidTokenException, InsufficientScopeException, InvalidRequestException, IOException {
 
         String to = "dmalexeydm@yandex.ru";
@@ -23,7 +22,7 @@ public class YaMoneyTest implements ApiTest2 {
         String messageFrom = "Комментарий к переводу, отображается в истории отправителя.";
         String messageTo = "Комментарий к переводу, отображается получателю.";
 
-        RequestPayment response = new YaService(CLIENT_ID).send(ACCESS_TOKEN, to, amount, messageFrom, messageTo);
+        RequestPayment response = new YaService().send(ACCESS_TOKEN, to, amount, messageFrom, messageTo);
 
         Assert.assertNotNull(response);
         Assert.assertEquals(response.getStatus(), BaseRequestPayment.Status.SUCCESS);
